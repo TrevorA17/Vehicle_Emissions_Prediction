9212e1dead85868f52e82f7849ce1f6abf335162
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-17 09:41:27 +0300

    Measures of Central Tendency

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -27,3 +27,29 @@ print(cylinder_freq)
 
 print("Frequency of Fuel Type:")
 print(fuel_type_freq)
+
+# Measures of Central Tendency
+mean_engine_size <- mean(vehicle_data$Engine_Size)
+median_engine_size <- median(vehicle_data$Engine_Size)
+
+mean_city_mpg <- mean(vehicle_data$City_MPG)
+median_city_mpg <- median(vehicle_data$City_MPG)
+
+mean_highway_mpg <- mean(vehicle_data$Highway_MPG)
+median_highway_mpg <- median(vehicle_data$Highway_MPG)
+
+mean_co2_emissions <- mean(vehicle_data$CO2_Emissions)
+median_co2_emissions <- median(vehicle_data$CO2_Emissions)
+
+print(paste("Mean Engine Size:", mean_engine_size))
+print(paste("Median Engine Size:", median_engine_size))
+
+print(paste("Mean City MPG:", mean_city_mpg))
+print(paste("Median City MPG:", median_city_mpg))
+
+print(paste("Mean Highway MPG:", mean_highway_mpg))
+print(paste("Median Highway MPG:", median_highway_mpg))
+
+print(paste("Mean CO2 Emissions:", mean_co2_emissions))
+print(paste("Median CO2 Emissions:", median_co2_emissions))
+
